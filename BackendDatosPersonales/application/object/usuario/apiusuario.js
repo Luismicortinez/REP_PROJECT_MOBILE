11d8ea6bb7233d3cb8a10cd3a346577f3e9176c5
lmcortinez <lmcortines@correo.iue.edu.co>
2025-05-13 16:18:01 -0500

    add api

--- BackendDatosPersonales/application/object/usuario/apiusuario.js
+++ BackendDatosPersonales/application/object/usuario/apiusuario.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { ModelUsuario, modelusuario } = require('../../../domain/object/usuario/modelusuario'); 
+const { ModelUsuario, modelusuario } = require('../../../domain/object/usuario/modelusuario');
 
 var router = express.Router();
 
@@ -17,13 +17,65 @@ var router = express.Router();
 router.get('/operaciongetusuario', async (req, res) => {
 	// var idusuario = "1";
 	var explanation = "102ds5455";
-	var	title = "Luis Miguel";
+	var title = "Luis Miguel";
 	// var apellidos = "Cortinez Rincon";
 	var url = "";
-	try {		
-        const modelusuario = new ModelUsuario(explanation, title, url);
-        res.status(200).json(modelusuario);	
+	try {
+		const modelusuario = new ModelUsuario(explanation, title, url);
+		res.status(200).json(modelusuario);
 
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+/**
+ * @swagger
+ * /operaciongetusuariodocumento:
+ *   get:
+ *     tags:
+ *       - Datos Personales
+ *     summary: Get ModelDatosPersonales by Documento
+ *     parameters:
+ *       - name: documento
+ *         in: query
+ *         description: Documento
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 0
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       404:
+ *         description: Usuario no encontrado
+ */
+router.get('/operaciongetusuariodocumento', async (req, res) => {
+
+	try {
+		const { documento, nombre } = req.query;
+
+		if (documento == '1037640956' && nombre == "miguel") {
+			res.status(200).json({
+				idusuario : "1",
+				documento: "1037640956",
+				nombres: "Luis Miguel",
+				apellidos : "Cortinez",
+				imagen : "https://media-bog2-1.cdn.whatsapp.net/v/t61.24694-24/491867909_9138957122875872_5802305768111151358_n.jpg?ccb=11-4&oh=01_Q5Aa1gHrqZYH8ay_4lI4dadMsFHziT2Qbs7fUBIbbiBbYu6PNQ&oe=6830C946&_nc_sid=5e03e0&_nc_cat=106"
+			});
+		}
+		else {
+			res.status(404).json({ message: 'Usuario no valido' });
+		}
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
---
+++ apiflutter/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consulta de Datos',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ConsultarDatos(),
+    );
+  }
+}
+
+class ConsultarDatos extends StatefulWidget {
+  const ConsultarDatos({super.key});
+
+  @override
+  _ConsultarDatosState createState() => _ConsultarDatosState();
+}
+
+class _ConsultarDatosState extends State<ConsultarDatos> {
+  TextEditingController documentoController = TextEditingController();
+  TextEditingController nombreController = TextEditingController();
+
+  String? nombres;
+  String? apellidos;
+  String? imagenUrl;
+  bool isLoading = false;
+
+  Future<void> consultarDatos() async {
+    final documento = documentoController.text;
+    final nombre = nombreController.text;
+
+    if (documento.isEmpty && nombre.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final url = 'http://192.168.202.130:8067/operaciongetusuariodocumento?documento=$documento&&nombre=$nombre'; // URL de la API remota
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          nombres = data['nombres'];
+          apellidos = data['apellidos'];
+          imagenUrl = data['imagen']; // URL de la imagen
+        });
+      } else {
+        throw Exception('Error en la consulta');
+      }
+    } catch (e) {
+      setState(() {
+        nombres = null;
+        apellidos = null;
+        imagenUrl = null;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error al consultar los datos')));
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Consulta de Datos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: documentoController,
+              decoration: InputDecoration(labelText: 'Ingrese la cédula'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: nombreController,
+              decoration: InputDecoration(labelText: 'Ingrese el nombre'),
+              keyboardType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: isLoading ? null : consultarDatos,
+              child: isLoading
+                  ? CircularProgressIndicator(color: Colors.white)
+                  : Text('Consultar'),
+            ),
+            SizedBox(height: 16),
+            if (nombres != null && apellidos != null) ...[
+              Text('Nombres: $nombres'),
+              Text('Apellidos: $apellidos'),
+              if (imagenUrl != null)
+                Image.network(imagenUrl!),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
